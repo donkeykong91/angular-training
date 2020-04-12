4cabf92e7574b4bcb59de2160feb89e0bcaf11f5
donkeykong91 <darylnator@gmail.com>
2020-04-12 06:03:42 -0700

    new files

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
-  name = 'The Redemption'
+  name = 'The Redemption';
 
   wasWatched() {
     return true;
