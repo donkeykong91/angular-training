87dfaa9044565a7ea90605f0b85de220d6e5ba47
donkeykong91 <darylnator@gmail.com>
2020-04-12 06:24:55 -0700

    added new files

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
-  onDelete(){
+  onDelete() {
     console.log('deleted');
   }
 }
